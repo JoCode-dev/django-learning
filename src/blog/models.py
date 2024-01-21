48a028ac42ef9b0b835377b13a9c5b2b6360e9da
JoCode <46027270+JoCode-dev@users.noreply.github.com>
2024-01-21 13:02:43 +0000

    Update BlogPost Model file

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,11 +1,37 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.text import slugify
 
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=36)
+    slug = models.SlugField()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+
+        super().save(*args, **kwargs)
+
+
 class BlogPost(models.Model):
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     published = models.BooleanField(default=False)
-    date = models.DateField(blank=True)
+    date = models.DateField(blank=True, null=True)
     content = models.TextField()
     description = models.TextField()
+
+    def publish_string(self):
+        if self.published:
+            return "L'article est publié"
+        return "L'article est inaccessible"
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+
+        super().save(*args, **kwargs)
